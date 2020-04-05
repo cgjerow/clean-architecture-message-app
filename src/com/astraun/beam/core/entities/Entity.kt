387c9ede60a432630d4b362218866141d288286c
connorjerow <cgjerow@gmail.com>
2020-04-05 15:35:09 -0500

    Create Entity base class.

---
+++ src/com/astraun/beam/core/entities/Entity.kt
@@ -0,0 +1,17 @@
+package com.astraun.beam.core.entities
+
+public abstract class Entity
+{
+    // I need to implement a factory to generate UUIDs as I don't want to build a dependency on a specific UUID implementation
+//    private val id: UUID
+//
+//    public constructor()
+//    {
+//        this.id = UUID()
+//    }
+//
+//    public constructor(id: String)
+//    {
+//        this.id = UUID(id)
+//    }
+}
