e5a3d3bd9d3727b19017fa8949ba83927340dc24
connorjerow <cgjerow@gmail.com>
2020-04-06 14:51:03 -0500

    add missing brace for interface syntax

--- src/com/astraun/beam/core/interactors/repository/UnitOfWork.kt
+++ src/com/astraun/beam/core/interactors/repository/UnitOfWork.kt
@@ -4,6 +4,7 @@ import com.astraun.beam.core.entities.Entity
 
 interface UnitOfWork
 {
+    fun register(repository: Repository<*>)
     fun registerNew(entity: Entity)
     fun registerDirty(entity: Entity)
     fun registerClean(entity: Entity)
